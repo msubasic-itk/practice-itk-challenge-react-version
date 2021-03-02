f28b18265212b10e68cfff05d09129cbd6077354
Milos Subasic <msubasic@itekako.com>
2021-03-02 17:05:57 +0100

    some general css improvements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import Grid from './Components/Grid/Grid';
 import List from './Components/List/List';
 import Map from './Components/Map/Map';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
+import './App.scss';
 
 function App() {
   return (
     <div>
       <header>
-        <ul>
-          <li><Link to='/'>Grid</Link></li>
-          <li><Link to='list'>List</Link></li>
-          <li><Link to='map'>Map</Link></li>
-        </ul>
+        <nav>
+          <ul>
+            <li><NavLink to='/' exact>Grid</NavLink></li>
+            <li><NavLink to='list'>List</NavLink></li>
+            <li><NavLink to='map'>Map</NavLink></li>
+          </ul>
+        </nav>
       </header>
-      <Route path='/' exact component={Grid} />
-      <Route path='/list' component={List} />
-      <Route path='/map' component={Map} />
+      <main>
+        <Route path='/' exact component={Grid} />
+        <Route path='/list' component={List} />
+        <Route path='/map' component={Map} />
+      </main>
     </div>
   );
 }
